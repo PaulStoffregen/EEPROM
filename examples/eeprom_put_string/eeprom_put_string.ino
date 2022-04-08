f38af72ba81edf5af4226ee765b3f98e4242386b
luni64 <12611497+luni64@users.noreply.github.com>
2022-04-08 17:24:22 +0200

    fixed wrong copy of example

--- examples/eeprom_put_string/eeprom_put_string.ino
+++ examples/eeprom_put_string/eeprom_put_string.ino
@@ -1,42 +1,58 @@
 /***
-    eeprom_get example.
+    eeprom_put_string example.
 
-    This shows how to use the EEPROM.get() method to read Arduino Strings from the EEPROM.
+    This shows how to use the EEPROM.put() method to write Arduino Strings to the EEPROM
+    Also, this sketch will pre-set the EEPROM data for the
+    example sketch eeprom_get_string.
 
-    To pre-set the EEPROM data, run the example sketch eeprom_put_string.
-    This sketch will run without it, however, the values shown
-    will be shown from what ever is already on the EEPROM.
-
-    This may cause the serial object to print out a large string
-    of garbage if there is no null character inside one of the strings
-    loaded.
-
-    Written by luni64 April 2022
+    Note, unlike the single byte version EEPROM.write(),
+    the put method will use update semantics. As in a String
+    will only be written to the EEPROM if the data is actually
+    different.
+    luni64, April 2022
     Released under MIT licence.
 ***/
+
 #include <EEPROM.h>
 
 void setup()
 {
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for boards with built in USB only
+    ; // wait for serial port to connect. Needed for boards with integrated USB only
   }
-  Serial.println("Read 6 Strings from EEPROM: "); //To pre-set the EEPROM data, run the example sketch eeprom_put_string.
 
-  String s;                     // Variable to store data read from EEPROM.
-  unsigned int eeAddress = 0;   // EEPROM address to start reading from
+  String s1 = "Hello EEPROM";   // Variable to write to EEPROM.
+  unsigned int eeAddress = 0;   // Location we want the data to be put.
 
-  EEPROM.get(eeAddress, s);     // Get the float data from the EEPROM at position 'eeAddress'
-  eeAddress += s.length() + 1;  // Advance read address to next String, add 1 to account for the leading \0
-  Serial.println(s);            // This may print print out a large string of garbage if there is no null character inside the string
+  EEPROM.put(eeAddress, s1);    // One simple call, with the address first and the object second.
+  eeAddress += s1.length() + 1; // Update address to the next byte after String s1.
+                                // Don't forget to add 1 to take the additionally written String terminator (\0) into account.
+  Serial.print("Written \"");
+  Serial.print(s1);
+  Serial.println("\" to EEPROM");
 
   for (int i = 0; i < 5; i++)
   {
-    EEPROM.get(eeAddress, s);
-    eeAddress += s.length() + 1;
-    Serial.println(s);
+    String s = "This is string #" + String(i);
+
+    if (s.length() + 1 <= EEPROM.length() - eeAddress) // Check for free EEPROM space.
+    {
+      EEPROM.put(eeAddress, s);     //Write to EEPROM.
+      eeAddress += s.length() + 1;  //Update address. Take the additionally written String terminator (\0) into account.
+
+      Serial.print("Written \"");
+      Serial.print(s);
+      Serial.println("\" to EEPROM");
+    }
+    else
+    {
+      Serial.println("No free space in EEPROM");
+      break;
+    }
   }
+
+  Serial.println("\nView the example sketch eeprom_get_string to see how you can retrieve the values!");
 }
 
 void loop() { /* Empty loop */
